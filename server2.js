6477a289e47d0e6f8048c383427a7f1c25b57bea
RAVINDRA JHA <ravijha110903@gmail.com>
2024-04-03 02:54:43 +0530

    node js

--- server2.js
+++ server2.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const fs = require('fs')
+const _ = require('lodash')
 const server = http.createServer((req,res)=>{
+  let num = _.random(0,50);
+  console.log(num)
+
+
   console.log(req.method , req.url)
+  fs.readFile("./index.html",(err,data) =>{
+    res.write(data)
+    res.end()
+  })
+ 
+
 })
 server.listen(3000,'localhost' , ()=>{
   console.log("listening on the port 3000")
   return
 
 })
+
