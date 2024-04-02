5d55578d30c875e9aeeb96fe26b63e78389cd448
RAVINDRA JHA <ravijha110903@gmail.com>
2024-04-03 00:00:37 +0530

    node js

--- server2.js
+++ server2.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-  console.log(req)
+  console.log(req.method , req.url)
 })
 server.listen(3000,'localhost' , ()=>{
   console.log("listening on the port 3000")
